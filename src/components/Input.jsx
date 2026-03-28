3991fbd7456f07fb49f66abd071d443fb1ab7b25
Arjuman Sultana <arjuman.sultana86@gmail.com>
2026-03-28 14:20:15 -0700

    Collecting user input with Refs & forwarded refs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,18 @@
-export default function Input({ label, textarea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
   const classes =
     "w-full px-3 py-2 rounded-md border border-stone-300 bg-stone-100 text-stone-700 focus:outline-none focus:border-stone-500 focus:bg-white";
   return (
     <p className="flex flex-col gap-1 my-4">
       <label className="text-sm font-semibold text-stone-600">{label}</label>
       {textarea ? (
-        <textarea className={classes} {...props} />
+        <textarea ref={ref} className={classes} {...props} />
       ) : (
-        <input className={classes} {...props} />
+        <input ref={ref} className={classes} {...props} />
       )}
     </p>
   );
-}
+});
+
+export default Input;
